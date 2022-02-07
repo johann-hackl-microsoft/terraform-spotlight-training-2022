715382c2e9aaf7b50a1f18380c220cc2031507cf
Johann Hackl <Johann.Hackl@microsoft.com>
2022-02-07 09:22:03 +0100

    initial

---
+++ Expressions-Basic/main.tf
@@ -0,0 +1,9 @@
+variable "x" {
+    type    = number
+    default = 2
+}
+variable "y" {
+    type    = number
+    default = 3
+}
+
---
+++ Expressions-Functions/main.tf
@@ -0,0 +1,22 @@
+variable "num1" {
+    type    = number
+    default = 2
+}
+variable "num2" {
+    type    = number
+    default = 3
+}
+
+variable "str1" {
+    type    = number
+    default = "Lorem ipsum dolor sit amet, consectetur adipisici elit"
+}
+variable "str2" {
+    type    = number
+    default = "xyz"
+}
+variable "str3" {
+    type    = number
+    default = "tet"
+}
+
